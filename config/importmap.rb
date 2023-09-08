7b390339ae1e1739fb752f56c55d1e4e62df1b4c
Leslie Rados <leslie.rados@gmail.com>
2023-09-08 18:34:06 +0200

    Bundler fix

--- config/importmap.rb
+++ config/importmap.rb
@@ -2,3 +2,4 @@
 
 pin "application", preload: true
 pin "flatpickr", to: "https://ga.jspm.io/npm:flatpickr@4.6.13/dist/esm/index.js"
+pin "@popperjs/core", to: "https://unpkg.com/@popperjs/core@2.11.2/dist/esm/index.js"
